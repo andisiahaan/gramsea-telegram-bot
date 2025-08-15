68c2fba502344db923d2e445cfef846f17004830
Andi Siahaan <andisiahaan670@gmail.com>
2025-08-15 15:57:49 +0700

    chore: deprecate Call class (merged into Gramsea)

--- src/Call.php
+++ src/Call.php
@@ -1,40 +1,16 @@
 <?php
 
-namespace Andisiahaan\GramseaTelegramBot;
+declare(strict_types=1);
 
-class Call
+namespace AndiSiahaan\GramseaTelegramBot;
+
+/**
+ * Deprecated placeholder. Call functionality was merged into `Gramsea`.
+ */
+final class Call
 {
-    protected string $baseUrl;
-
-    public function __construct(string $botToken)
+    public function __construct(...$args)
     {
-        $this->baseUrl = "https://api.telegram.org/bot{$botToken}/";
+        throw new \BadMethodCallException('Call is deprecated; use Gramsea instead.');
     }
-    
-    /**
-     * Magic method to directly call Telegram API methods
-     *
-     * @param string $method Telegram Bot API method name
-     * @param array $arguments Arguments passed to the method (first argument must be an array of parameters)
-     * @return array Response from Telegram API
-     */
-    public function __call(string $method, array $arguments): array
-    {
-        $parameters = $arguments[0] ?? [];
-        return $this->callMethod($method, $parameters);
-    }
-
-    /**
-     * General method to access all Telegram Bot API methods
-     *
-     * @param string $method Telegram Bot API method name
-     * @param array $parameters Parameters for the method
-     * @return array Response from Telegram API
-     */
-    public function callMethod(string $method, array $parameters = []): array
-    {
-        return Curl::request($this->baseUrl . $method, $parameters);
-    }
-
-
 }
